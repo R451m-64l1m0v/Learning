6dbe61e64952242e477949c2f9c74162a75baa44
R451m-64l1m0v <rasimg036@gmail.com>
2021-09-16 00:12:09 +0300

    Приволит список врачей к static class

--- RegisterToDoc/Services/DoctorDataService.cs
+++ RegisterToDoc/Services/DoctorDataService.cs
@@ -6,65 +6,97 @@ using RegisterToDoc.Models;
 
 namespace RegisterToDoc.Services
 {
-    public class DoctorDataService
+    public static class DoctorDataService
     {
-        public List<Doctor> GetDoctorsList()
+        public static List<Doctor> Doctors = new List<Doctor>
         {
-            var list = new List<Doctor>();
-            var doctor1 = new Doctor();
-            var doctor2 = new Doctor();
-            var doctor3 = new Doctor();
-            var doctor4 = new Doctor();
-            var doctor5 = new Doctor();
+            new Doctor()
+            {
+                Id = 1,
+                Name = "Vasyan1",
+                Surname = "111",
+                Age = 36,
+                Specialization = "Проктолог",
+                Experience = 17,
+                Education = "Высшее",
+                WorkTimeFull = new List<WorkTime>()
+                {
+                    new WorkTime()
+                    {
+                        StartHour = 8,
+                        EndHour = 17,
+                        Number = 16,
+                    },
+                    new WorkTime()
+                    {
+                        StartHour = 8,
+                        EndHour = 17,
+                        Number = 17,
+                    },
+                    new WorkTime()
+                    {
+                        StartHour = 8,
+                        EndHour = 17,
+                        Number = 18,
+                    },
+                    new WorkTime()
+                    {
+                        StartHour = 8,
+                        EndHour = 17,
+                        Number = 19,
+                    },
+                    new WorkTime()
+                    {
+                        StartHour = 8,
+                        EndHour = 17,
+                        Number = 20,
+                    }
+                },
+            },
 
-            doctor1.Id = 1;
-            doctor1.Name = "1";
-            doctor1.Surname = "111";
-            doctor2.Age = 36;
-            doctor1.Specialization = "Проктолог";
-            doctor1.Experience = 17;
-            doctor1.Education = "Высшее";
+            new Doctor()
+            {
+                Id = 2,
+                Name = "2",
+                Surname = "222",
+                Age = 36,
+                Specialization = "Педиатр",
+                Experience = 8,
+                Education = "Высшее",
+            },
 
-            doctor2.Id = 2;
-            doctor2.Name = "2";
-            doctor2.Surname = "222";
-            doctor2.Age = 36;
-            doctor2.Specialization = "Педиатр";
-            doctor2.Experience = 8;
-            doctor2.Education = "Высшее";
+            new Doctor()
+            {
+                Id = 3,
+                Name = "3",
+                Surname = "333",
+                Age = 60,
+                Specialization = "Хирург",
+                Experience = 25,
+                Education = "Не ПТУ, а колледж",
+            },
 
-            doctor3.Id = 3;
-            doctor3.Name = "3";
-            doctor3.Surname = "333";
-            doctor3.Age = 60;
-            doctor3.Specialization = "Хирург";
-            doctor3.Experience = 25;
-            doctor3.Education = "Не ПТУ, а колледж";
+            new Doctor()
+            {
+                Id = 4,
+                Name = "4",
+                Surname = "444",
+                Age = 30,
+                Specialization = "Проктолог",
+                Experience = 5,
+                Education = "Высшее",
+            },
 
-            doctor4.Id = 4;
-            doctor4.Name = "4";
-            doctor4.Surname = "444";
-            doctor4.Age = 30;
-            doctor4.Specialization = "Проктолог";
-            doctor4.Experience = 5;
-            doctor4.Education = "Высшее";
-
-            doctor5.Id = 5;
-            doctor5.Name = "5";
-            doctor5.Surname = "555";
-            doctor5.Age = 30;
-            doctor5.Specialization = "Хирург";
-            doctor5.Experience = 5;
-            doctor5.Education = "Высшее";
-
-
-            list.Add(doctor1);
-            list.Add(doctor2);
-            list.Add(doctor3);
-            list.Add(doctor4);
-            list.Add(doctor5);
-
-            return list;
-        }
+            new Doctor()
+            {
+                Id = 5,
+                Name = "5",
+                Surname = "555",
+                Age = 30,
+                Specialization = "Хирург",
+                Experience = 5,
+                Education = "Высшее",
+            },
+        };
     }
 }
